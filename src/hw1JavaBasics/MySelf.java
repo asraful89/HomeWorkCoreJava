13fd37a39248e316a97326c801e3b49fa9df2217
asraful89 <Islmasrfl@gmail.com>
2022-05-16 18:50:09 -0400

    first commit

---
+++ src/hw1JavaBasics/MySelf.java
@@ -0,0 +1,14 @@
+package hw1JavaBasics;
+
+/*Create a Project name -->  "HomeWorkCoreJava", package name --> "hw1JavaBasics", Class name "MySelf" (Don't select the main method while creating Class). Can you get the main method by a short cut? If yes, Please do so. Then, print Your name and address in different lines (try to follow how to copy and paste a line in IDE by key board command). Now, Use multiple line comments between package and Class. Write there the naming convention of Project, package and Class. (please get the help of class note). Use also single line comment just before the main method and write about what you know about main method (please get the help of class note). Organize the code (shift+control/command+F) and paste below.
+
+ */
+
+public class MySelf {
+
+	public static void main(String[] args) {
+		System.out.println("Asraful islam");
+		System.out.println("12934 champaign ave warren");
+	}
+
+}
